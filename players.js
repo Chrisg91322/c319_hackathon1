a606a4cacd061c6e0903caa7589a75c6eadd0d80
Christopher Gormley <Chrisg913@gmail.com>
2019-04-17 16:58:38 -0700

    created player and frog js script

---
+++ players.js
@@ -0,0 +1,30 @@
+class Players{
+    constructor(playerName, image){
+        this.name = playerName;
+        this.image = image;
+        this.points = {
+            frogsJumped: 0,
+            trophies: 0
+        }
+        this.domElement = null;
+        this.currentRider
+    debugger;
+    if(this.name == 'Green Rider'){
+        return name++;
+        }else{
+            return name++;   
+        }
+        this.currentRider.changePoints(trophies);
+        }
+      
+
+    changeTurns(){
+        this.playerTurn = !this.playerTurn;
+    }
+}
+
+var player = [new Players("Green Rider"), new Players("Purple Rider")];
+var playerTurn = true;
+var currentRider = player[0];
+var opposingRider = player[1];
+
